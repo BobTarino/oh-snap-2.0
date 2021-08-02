dfd79054c5b647bef5f11a4d692c8c46d7ae5883
Bobby T <bob.tarino@gmail.com>
2021-08-02 12:15:08 -0500

    created Nav component

--- photo-port/src/App.js
+++ photo-port/src/App.js
@@ -2,13 +2,14 @@
 // **You need to import React in every component file. 
 import React from 'react';
 import './App.css';
+import Nav from './components/Nav';
 import About from './components/About';
 
 
 function App() {
-
   return (
     <div>
+      <Nav></Nav>
       <main>
         <About></About>
       </main>
