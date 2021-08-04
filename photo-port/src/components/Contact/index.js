eff4de2fcc8edbcc785438691cb4a2d4795d9e4f
Bobby T <bob.tarino@gmail.com>
2021-08-04 16:08:22 -0500

    control state with controlled component

--- photo-port/src/components/Contact/index.js
+++ photo-port/src/components/Contact/index.js
@@ -1,19 +1,36 @@
+import React, { useState } from 'react';
+
+
 function ContactForm() {
+    //  hook will manage form data and initialize value of the state
+    const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+    const { name, email, message } = formState;
+    // function will sync the internal state of the component formState with the user input from the DOM
+    function handleChange(e) {
+        // uses setFormState() to update the formState value for the name property
+        // spread operator (...) to retain the other key-value pairs in this object
+        setFormState({...formState, [e.target.name]: e.target.value })
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(formState);
+    }
+      
     return (
         <section>
           <h1>Contact me</h1>
-          <form id="contact-form">
+          <form id="contact-form" onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name:</label>
-                    <input type="text" name="name" />
+                    <input type="text" defaultValue={name} onChange={handleChange} name="name" />
                 </div>
                 <div>
                     <label htmlFor="email">Email address:</label>
-                    <input type="email" name="email" />
+                    <input type="email" defaultValue={email} name="email" onChange={handleChange} />
                 </div>
                 <div>
                     <label htmlFor="message">Message:</label>
-                    <textarea name="message" rows="5" />
+                    <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
                 </div>
                 <button type="submit">Submit</button>
           </form>
